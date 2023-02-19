e214a888bd887ccacb58fc2550d95f503b8a71f2
Azamat Nurov <azamat@mancho.dev>
2023-02-19 11:54:46 +0600

    init project

---
+++ src/pages/basket/Basket.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Basket() {
+  return (
+    <section>
+      <Box>Basket</Box>
+    </section>
+  );
+}
---
+++ src/redux/Store.ts
@@ -0,0 +1,14 @@
+/* External dependencies */
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const setUpStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setUpStore>;
+export type AppDispatch = AppStore["dispatch"];
