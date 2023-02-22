f89e5a0ac463bc070fed69847edcda751ff4c4d0
Azamat Nurov <azamat@mancho.dev>
2023-02-22 17:29:33 +0600

    add function prev next

--- src/components/trackList/TrackList.tsx
+++ src/components/trackList/TrackList.tsx
@@ -1,14 +1,19 @@
 import { Box, Text } from "@chakra-ui/react";
-import { useAppSelector } from "../../hooks/Index";
 import { useAction } from "../../hooks/useActions";
 import { ITrack } from "../../redux/types/Track";
 import MusicForList from "../ui/MusicForList";
 
 interface ITrackList {
   tracks: ITrack[];
+  setCurrentIndex?: any;
+  setEventChange?: any;
 }
 
-export default function TrackList({ tracks }: ITrackList) {
+export default function TrackList({
+  tracks,
+  setCurrentIndex,
+  setEventChange,
+}: ITrackList) {
   const { activeTrack } = useAction();
 
   const listTable = [
@@ -26,15 +31,19 @@ export default function TrackList({ tracks }: ITrackList) {
     },
   ];
 
-  const OnChange = (data: ITrack) => {
+  const OnChange = (data: ITrack, index: any) => {
+    setCurrentIndex(index);
     activeTrack(data);
+    setEventChange(true);
   };
 
+  // tracks.length === index + 1 ? index : index + 1;
+
   return (
-    <Box>
+    <Box bg="transparent">
       <Box display="flex" justifyContent="space-between">
         {listTable.map((item, index) => (
-          <Text key={index} fontWeight="600">
+          <Text key={index} fontWeight="600" color="white">
             {item.text}
           </Text>
         ))}
@@ -45,8 +54,11 @@ export default function TrackList({ tracks }: ITrackList) {
             name={el?.name}
             key={index}
             music={el}
-            onClick={OnChange}
+            onClick={() => OnChange(el, index)}
           />
+        ))}
+        {tracks.map((el) => (
+          <Box>{el.name}</Box>
         ))}
       </Box>
     </Box>
--- src/pages/allPlaylist/AllPlaylist.tsx
+++ src/pages/allPlaylist/AllPlaylist.tsx
@@ -1,34 +1,71 @@
 import { Container } from "@chakra-ui/react";
+import { useState } from "react";
 
 import AudioPlayer from "../../components/audio-player/AudioPlayer";
 import TrackList from "../../components/trackList/TrackList";
 
 export default function AllPlaylist() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [eventChange, setEventChange] = useState(false);
+
   const listTruck = [
     {
       _id: "1",
       name: "Волчий вой",
       audio:
         "https://muzes.net/uploads/music/2022/10/Ulukmanapo_Volchij_voj.mp3",
+      artist: "Ulukmanapo",
+      text: "03:11",
     },
     {
       _id: "2",
       name: "la liga",
       audio: "https://dl2.mp3party.net/online/10068051.mp3",
+      artist: "Ulukmanapo",
+      text: "03:11",
     },
     {
       _id: "3",
       name: "Герой",
       audio:
         "https://uztop.net/uploads/music/2023/02/FREEMAN_996_Geroj_OST_RAZBOI.mp3",
+      artist: "freeman 996",
+      text: "03:07",
+    },
+    {
+      _id: "4",
+      name: "Албетте",
+      audio: require("../../assets/audio/freeman996-albette.mp3"),
+      artist: "freeman 996",
+      text: "03:07",
+    },
+    {
+      _id: "5",
+      name: "Ойлорумда",
+      audio:
+        "https://mp3fly.net/uploads/files/mp3/02-2021/1613108060_Bakr_-_Oylorumda.mp3",
+      artist: "freeman 996",
+      text: "03:07",
     },
   ];
 
+  console.log(currentIndex, "DOM");
+
   return (
-    <section>
+    <section style={{ background: "#242424" }}>
       <Container maxW="1220px">
-        <AudioPlayer listTruck={listTruck} />
-        <TrackList tracks={listTruck} />
+        <AudioPlayer
+          setEventChange={setEventChange}
+          eventChange={eventChange}
+          listTruck={listTruck}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+        <TrackList
+          tracks={listTruck}
+          setCurrentIndex={setCurrentIndex}
+          setEventChange={setEventChange}
+        />
       </Container>
     </section>
   );
