4d955b9c2a6553d82deabbd9faf625fae5eea770
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2023-03-13 22:44:34 +0600

    google auth

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import { theme } from "./components/layout/Index";
 import PageRouting from "./components/layout/PageRouting";
@@ -13,16 +14,22 @@ import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const idClient =
+  "464094313649-paipogb1oimb09fv7c4nhfhcbv1fraqi.apps.googleusercontent.com";
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <Provider store={setUpStore()}>
-          <PageRouting>
-            <App />
-          </PageRouting>
-        </Provider>
-      </ChakraProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={idClient}>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <Provider store={setUpStore()}>
+            <PageRouting>
+              <App />
+            </PageRouting>
+          </Provider>
+        </ChakraProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
