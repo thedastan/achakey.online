b11eb0b41a468e192a463134a8f273a4da60aad7
maksat-01 <maksatarykbaiuulu@gmail.com>
2023-03-23 18:02:07 +0600

    change disk style adaptive

--- src/components/Home/Music/Disk/index.tsx
+++ src/components/Home/Music/Disk/index.tsx
@@ -11,12 +11,12 @@ interface MusicProps {
 const Disk: React.FC<MusicProps> = ({musicPlay}) => {
     const breakpoints = useBreakpointValue({base: "base", sm: "sm", md: "md", lg: "lg", xl: "xl", "2xl": "2xl"});
     return (
-        <Box style={{display: musicPlay ? "block" : "none"}}>
+        <Box style={{display: musicPlay ? "block" : "none"}}   >
             <Image
                 src={disk}
                 alt="disk"
                 className="disk"
-                display={{base: "none", sm: "none", md: "none", lg: "block","2xl":"block"}}
+                display={{base: "none", sm: "none", md: "none", lg: "block" ,xl:"block"}}
                 top={{lg: "43%", xl: "43%", "2xl": "43%"}}
                 width={breakpoints === "lg" ? "140px" : "170px" && breakpoints === "xl" ? "200px" : "221px"}
             />
--- src/components/Home/Music/index.tsx
+++ src/components/Home/Music/index.tsx
@@ -189,7 +189,6 @@ const Music: React.FC<MusicProps> = ({ musicPlay }) => {
 
   return (
     <Box
-      maxW="3072px"
       width="100%"
       className="music"
       bg="red"
