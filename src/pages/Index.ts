d72a1caeda8e74f7e8213a934141ee0eaa6d2a60
Azamat Nurov <azamat@mancho.dev>
2023-05-10 11:33:19 +0600

    Жон эле текшеруу

--- src/pages/Index.ts
+++ src/pages/Index.ts
@@ -1,16 +1,26 @@
+//accountManagement
 export { default as AccountManagamentPage } from "./accountManagement/AccountManagamentPage";
 export { default as ChangePasswordPage } from "./accountManagement/ChangePasswordPage";
+
+//components
 export { default as Header } from "../components/header/Header";
-export { default as AllPlaylist } from "../containers/excerptPlaylist/AllPlaylist";
-export { default as Home } from "../containers/Home/index";
 export { default as PrivacyPolicy } from "../components/footer/PrivacyPolicy";
 export { default as PublicOffer } from "../components/footer/PublicOffer";
 export { default as SearchResult } from "../components/search-result/SearchResult";
-export { default as MyPlaylist } from "./myPlaylist/MyPlaylist";
+
+//containers
+export { default as AllPlaylist } from "../containers/excerptPlaylist/AllPlaylist";
+export { default as Home } from "../containers/Home/index";
+
+//resetPassword
 export { default as ResetPasswordEmailPage } from "./resetPassword/ResetPassword";
 export { default as ResetPasswordPhonePage } from "./resetPassword/ResetPasswordPhonePage";
+
+//global-audio
 export { default as Navigation } from "../global-audio-player-excerpt/AudioPlayerExcerpt";
 export { default as AudioPlayerBottom } from "../global-audio-player/audioPlayerBottom";
+
+export { default as MyPlaylist } from "./myPlaylist/MyPlaylist";
 export { default as Basket } from "./basket/Basket";
 export { default as NotFound } from "./404/NotFound";
 export { default as DetailsAlbums } from "./detailsAlbums/DetailsAlbums";
