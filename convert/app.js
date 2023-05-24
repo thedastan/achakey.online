9eaf67c086fbca9b772ab6860b49d0fa8ed64d4c
thedastan <dastanshermatov@gmail.com>
2023-05-24 12:38:01 +0600

    Developing converting txt to mp3

--- convert/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const router = require('./router/file.route')
-
-
-const PORT=8000
-app.use('/api', router)
-
-app.listen(PORT, () => console.log(`Server started on port ... ${PORT}`))
